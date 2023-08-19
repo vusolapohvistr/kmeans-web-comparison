950e5f1f7eb0af5008239b06df18e42f866f970f
Serhii Pylypchuk <spylypchuk@s-pro.io>
2023-08-19 19:44:13 +0200

    feature: use canvas to 'show' clusterisation results

--- src/index.ts
+++ src/index.ts
@@ -1,164 +1,88 @@
 import skmeans from "skmeans";
 import kmeansWasm from "kmeans-wasm";
 
-const kTests = [2, 10, 50];
-const dimensionsTests = [3, 10, 50];
-const dataSize = 10000;
-const maxIterations = 1000;
-
-interface ITestResult {
-  k: number;
-  dimensions: number;
-  avarageTime: number;
-}
-
 (async () => {
   const wasmKmeansFunction = (await kmeansWasm).kmeans;
   const wasmRgbKmeansFunction = (await kmeansWasm).kmeans_rgb;
 
-  const testsData: {
-    k: number;
-    dimensions: number;
-    data: number[][];
-  }[] = kTests.map(k =>
-    dimensionsTests.map(dimensions => {
-      return {
-        k,
-        dimensions,
-        data: generateData(dataSize, dimensions),
-      };
-    })
-  ).flat();
+  const dataset = getMickyMouseDataset();
 
-  const skmeansResults: ITestResult[] = testsData.map(({ k, dimensions, data }) => ({
-    k,
-    dimensions,
-    avarageTime: test(skmeans, 10, data, k, undefined, maxIterations),
-  }));
+  const skmeansTime = Date.now();
+  const resultSkmeans = skmeans(dataset, 3, null, 10000);
+  document.getElementById('skmeans-result')!.innerText = `skmeans took ${Date.now() - skmeansTime}ms`;
+  drawResults('skmeans', dataset, resultSkmeans.idxs);
 
-  const kmeansWasmResults: ITestResult[] = testsData.map(({ k, dimensions, data }) => ({
-    k,
-    dimensions,
-    avarageTime: test(wasmKmeansFunction, 10, data, k, maxIterations),
-  }));
-
-  // Update the results table
-  const resultsTable = document.getElementById("results-table");
-  if (resultsTable) {
-    resultsTable.innerHTML += `
-      <h2>skmeans (data size - ${dataSize}, max iterations: ${maxIterations})</h2>
-      ${generateTable(skmeansResults)}
-      <h2>kmeans (data size - ${dataSize}, max iterations: ${maxIterations})</h2>
-      ${generateTable(kmeansWasmResults)}
-    `;
-  }
-
-  // Download the image
-  const image = new Image();
-  image.src = "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80";
-  image.crossOrigin = "Anonymous";
-  image.onload = () => {
-    const canvas = document.createElement("canvas");
-    canvas.width = image.width;
-    canvas.height = image.height;
-    const ctx = canvas.getContext("2d");
-    ctx!.drawImage(image, 0, 0);
-
-    const imageData = ctx!.getImageData(0, 0, 300, 300);
-    const rgbaData = imageData.data;
-
-    const rgbPointsData: number[][] = [];
-    for (let i = 0; i < rgbaData.length; i += 4) {
-      rgbPointsData.push([rgbaData[i], rgbaData[i + 1], rgbaData[i + 2]]);
-    }
-
-    const skmeansRGBresult = kTests.map(k => ({
-      k,
-      dimensions: 3,
-      avarageTime: test(skmeans, 2, rgbPointsData, k, undefined, maxIterations),
-    }));
-
-    const rgbData: number[] = [];
-    for (let i = 0; i < rgbaData.length; i += 4) {
-      rgbData.push(rgbaData[i], rgbaData[i + 1], rgbaData[i + 2]);
-    }
-    const rgbUint8Array = new Uint8Array(rgbData);
-
-    const kmeansWasmRGBresult = kTests.map(k => ({
-      k,
-      dimensions: 3,
-      avarageTime: test(wasmRgbKmeansFunction, 2, rgbUint8Array, k, maxIterations),
-    }));
-
-    // Update the results table
-    if (resultsTable) {
-      resultsTable.innerHTML += `
-      <h2>skmeans RGB</h2>
-      ${generateTable(skmeansRGBresult)}
-      <h2>kmeans RGB</h2>
-      ${generateTable(kmeansWasmRGBresult)}
-      `;
-    }
-  };
+  const kmeansWasmTime = Date.now();
+  const resultKmeansWasm = wasmKmeansFunction(dataset, 3, 10000, 0.00000000001);
+  document.getElementById('kmeans-wasm-result')!.innerText = `kmeans-wasm took ${Date.now() - kmeansWasmTime}ms`;
+  drawResults('kmeansWasm', dataset, resultKmeansWasm.idxs);
 })();
 
-function test<Fn extends (...args: Args) => Res, Args extends unknown[], Res>(fn: Fn, times: number, ...args: Args): number {
-  let sum = 0;
-  for (let i = 0; i < times; i++) {
-    const start = performance.now();
-    const res = fn(...args);
-    sum += performance.now() - start;
-    console.log((res as any)[0]?.toString());
+type Pixel = [x: number, y: number];
+
+function drawResults(canvasId: string, pixels: Pixel[], idxs: number[]) {
+  const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+  const height = canvas.height;
+  const width = canvas.width;
+
+  const ctx = canvas.getContext('2d')!;
+
+  const leftPixel = pixels.reduce((acc, val) => val[0] < acc[0] ? val : acc);
+  const rightPixel = pixels.reduce((acc, val) => val[0] > acc[0] ? val : acc);
+  const topPixel = pixels.reduce((acc, val) => val[1] > acc[1] ? val : acc);
+  const bottomPixel = pixels.reduce((acc, val) => val[1] < acc[1] ? val : acc);
+  console.log(leftPixel, rightPixel, topPixel, bottomPixel);
+
+  function xToCanvasX(x: number): number {
+    return Math.floor(width * ((x - leftPixel[0]) / (rightPixel[0] - leftPixel[0])));
   }
 
-  return sum / times;
+  function yToCanvasY(y: number): number {
+    return Math.floor(height * ((y - bottomPixel[1]) / (topPixel[1] - bottomPixel[1])));
+  }
+
+  function pixelToCanvasPixel(pixel: Pixel): Pixel {
+    return [xToCanvasX(pixel[0]), yToCanvasY(pixel[1])];
+  }
+
+  function fillCanvasPixel(pixel: Pixel, color: number) {
+    if (color === 0) {
+      ctx.fillStyle = "red";
+    } else if (color === 1) {
+      ctx.fillStyle = "blue";
+    } else if (color === 2) {
+      ctx.fillStyle = "green";
+    } else {
+      ctx.fillStyle = "black";
+    }
+    ctx.fillRect(pixel[0], pixel[1], 2, 2);
+  }
+
+  for (const [i, pixel] of pixels.entries()) {
+    const canvasPixel = pixelToCanvasPixel(pixel);
+    fillCanvasPixel(canvasPixel, idxs[i]);
+  }
 }
 
-// Replace this function with your data generation function or provide a fixed dataset
-function generateData(size: number, dimensions: number): number[][] {
-  return new Array(size)
-    .fill(0)
-    .map(() =>
-      new Array(dimensions)
-        .fill(0)
-        .map(() => Math.random())
-    );
+function getMickyMouseDataset(): Pixel[] {
+  const leftUpCircle = getRandomPixelsInCircle([-1, 2], 1, 10000);
+  const rightUpCircle = getRandomPixelsInCircle([1, 2], 1, 10000);
+  const centreCircle = getRandomPixelsInCircle([0, 0], 2, 20000);
+
+  return leftUpCircle.concat(rightUpCircle).concat(centreCircle);
 }
 
-function generateTable(data: ITestResult[]) {
-  const kValues = Array.from(new Set(data.map((item) => item.k))).sort((a, b) => a - b);
-  const dimensionValues = Array.from(new Set(data.map((item) => item.dimensions))).sort((a, b) => a - b);
+function getRandomPixelsInCircle(centre: Pixel, radius: number, pixelsCount: number): Pixel[] {
+  return new Array(pixelsCount).fill(0).map(() => getRandomPixelInCircle(centre, radius));
+}
 
-  let table = `
-    <table>
-      <thead>
-        <tr>
-          <th>Dimensions \\ k</th>
-  `;
+function getRandomPixelInCircle(centre: Pixel, radius: number): Pixel {
+  const distanceToCentre = Math.random() * radius;
+  const angle = Math.random() * 2 * Math.PI;
+  
+  // y  / distanceToCentre = sin(angle). y = distanceToCentre * sin(angle)
+  const x = centre[0] + distanceToCentre * Math.sin(angle);
+  const y = centre[1] + distanceToCentre * Math.cos(angle);
 
-  kValues.forEach((k) => {
-    table += `<th>${k}</th>`;
-  });
-
-  table += `
-        </tr>
-      </thead>
-      <tbody>
-  `;
-
-  dimensionValues.forEach((dimension) => {
-    table += `<tr><td>${dimension}</td>`;
-    kValues.forEach((k) => {
-      const item = data.find((item) => item.k === k && item.dimensions === dimension);
-      table += `<td>${item ? item.avarageTime : ''}</td>`;
-    });
-    table += `</tr>`;
-  });
-
-  table += `
-      </tbody>
-    </table>
-  `;
-
-  return table;
+  return [x, y];
 }
